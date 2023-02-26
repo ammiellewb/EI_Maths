78e5c49aa6400f2330025bee851c6a04524f3815
Ammielle WB <61096548+ammiellewb@users.noreply.github.com>
2023-02-26 02:19:03 -0500

    Update 2Script_de_programmation_Python.py

--- 2Script_de_programmation_Python.py
+++ 2Script_de_programmation_Python.py
@@ -1,11 +1,11 @@
 import numpy as np #  pour les opérations mathématiques
 import matplotlib.pyplot as plt # pour tracer les graphiques
 import random
-from scipy.integrate import odeint # pour comparer les résultats stochastiques et déterministes
+from scipy.integrate import odeint # pour résoudre les problèmes de valeurs initiales des EDO dans le MSIRD
 
 # MODÈLE STOCHASTIQUE
 # définir les conditions initiales
-# lorsque le nombre de personnes dans ces groupes change, de nouvelles valeurs seront ajoutées aux listes.
+# lorsque le nombre de personnes dans ces groupes change, de nouvelles valeurs seront ajoutées aux listes
 S = [1197] 
 I = [3]
 R = [0]
@@ -15,8 +15,8 @@ t = [0]
 tend = 1000
 
 # définir les paramètres
-ab = 40 * 0.001
-r = 0.01
+beta = 40 * 0.001
+gamma = 0.01
 
 
 # boucle de la simulation de l'algorithme de Gillespie
@@ -25,13 +25,13 @@ while t[-1] < tend and (S[-1] + I[-1] >= 1):
     N = S[-1] + I[-1] + R[-1]
 
      # liste des propensions des événements à se produire 
-    props = [ab*I[-1]*S[-1]/N, r*I[-1]]
+    props = [beta*I[-1]*S[-1]/N, r*I[-1]]
 
     # calculer la somme des probabilités
     prop_sum = sum(props)
 
     # définir l'intervalle entre le moment présent et le moment de l'événement suivant
-    # qui sera tiré d'une distribution aléatoire dépendant de la somme des propensions que ces événements se produisent.
+    # qui sera tiré d'une distribution aléatoire dépendant de la somme des propensions que cet événement se produit.
     tau = np.random.exponential(scale=1/prop_sum)
     t.append(t[-1]+tau)
 
@@ -44,14 +44,14 @@ while t[-1] < tend and (S[-1] + I[-1] >= 1):
             I.append(I[-1] + 1)
             R.append(R[-1])
     
-    # si la première affirmation est fausse,  alors ce n'est pas le cas du premier événement et le deuxième événement doit se produire
+    # si ce n'est pas le cas du premier événement,  alors le deuxième événement doit se produire
     # un retrait : Infecté devient Récupéré
-    else: 
+    else:
             S.append(S[-1])
             I.append(I[-1] - 1)
             R.append(R[-1] + 1)    
 
-
+            
 
 # tracer un graphique pour chaque variable du modèle
 f,(ax1,ax2,ax3) = plt.subplots(3)
@@ -67,20 +67,19 @@ ax2.set_ylabel("I")
 ax3.set_ylabel("R")
 ax3.set_xlabel("Temps")
 
-# plt.show()
-
 
 # MODÈLE DÉTERMINISTE
+# fixer le point limite de t
 t = np.linspace(0,tend, num=1000)
 
 
-params = [a*b,r]
+params = [beta,gamma]
 
-
+# conditions initiales
 y0 = [1197, 3, 0]
 
 
-
+# créer une fonction 
 def sim(variables, t, params):
 
     S = variables[0]
@@ -89,16 +88,17 @@ def sim(variables, t, params):
 
     N = S + I + R
 
-    ab = params[0]
-    r = params[1]
+    beta = params[0]
+    gamma = params[1]
 
-    dSdt = -ab * I * S / N
-    dIdt = ab * I * S / N - r * I
-    dRdt = r * I
+    # faire appel aux dérivées de chaque variable
+    dSdt = -beta * I * S / N
+    dIdt = beta * I * S / N - r * I
+    dRdt = gamma * I
 
     return([dSdt, dIdt, dRdt])
 
-
+# résoudre un système des EDO
 y = odeint(sim, y0, t, args=(params,))
 
 
@@ -106,5 +106,5 @@ line1, = ax1.plot(t,y[:,0]) # S
 line2, = ax2.plot(t,y[:,1]) # I
 line3, = ax3.plot(t,y[:,2]) # R
 
-
+# montrer les graphiques des résultats déterministes (orange) et stochastiques (bleu)
 plt.show()
